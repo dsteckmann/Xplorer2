ba7f020707f191aa8a3b6d99cf9d418c77cb86fb
dsteckmann <94651919+dsteckmann@users.noreply.github.com>
2025-10-24 15:27:35 -0400

    Delete Xplorer 2 REV 1_24.cydwr directory

--- Xplorer 2 REV 1_24.cydwr/Relative Address Xplorer2update.bat
+++
@@ -1,6 +0,0 @@
-
-Copy "%~dp0\CortexM3\ARM_GCC_541\Debug\Xplorer 2 REV 1_24.cyacd" "%~dp0\Xplorer2update.cyacd"
-
-
-::PAUSE
-:: Give the user some time to see the results. Because this is our last line, the program will exit and the command window will close once this line finishes. 
